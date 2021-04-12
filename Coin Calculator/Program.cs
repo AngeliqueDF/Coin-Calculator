f1ffb0590d644abccb738ac1671c30062145153d
Angélique Faye <angelique.d.faye@gmail.com>
2021-04-12 23:28:35 +0200

    values types, formatting input

--- Coin Calculator/Program.cs
+++ Coin Calculator/Program.cs
@@ -1,32 +1,72 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Coin_Calculator
 {
     class MainClass
-  {
-    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to Money Maker!");
-      Console.WriteLine("Enter the amount to convert");
-      double amount = Math.Round(Convert.ToDouble(Console.ReadLine()));
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Coin Calculator!");
+            Console.WriteLine("Enter the amount to convert");
 
-      Console.WriteLine($"{amount} cents is equal to...");
+            string enteredNumber = Console.ReadLine();
+            decimal amount = 0;
 
-      int goldValue = 10;
-      int silverValue = 5;
+            //if user enters . replace it with comma
+            string pattern = @"\.";
+            Regex rx = new Regex(pattern);
+            bool match = rx.IsMatch(enteredNumber);
+            if(match == true)
+            {
+                string formattedInput = rx.Replace(enteredNumber, ",");
+                amount = Convert.ToDecimal(formattedInput);
+            }
+            else
+            {
+                amount = Convert.ToDecimal(enteredNumber);
+            }
 
-      // find the maximum number of gold coins that fit into the amount
-      double goldCoins = Math.Floor(amount/goldValue);
-      double remainder = amount % goldValue;
 
-      // find the maximum number of silver coins that fit into the remainder
-      double silverCoins = Math.Floor(remainder / silverValue);
-      remainder = remainder % silverValue;
+            Console.WriteLine($"{amount} cents is equal to...");
 
-      Console.WriteLine($"Gold coins: {goldCoins}");
-      Console.WriteLine($"Silver coins: {silverCoins}");
-      Console.WriteLine($"Remainder: {remainder}");
+            //value
+            decimal goldValue = 10;
+            decimal silverValue = 5;
+            decimal bronzeValue = 2.5m;
 
+            decimal remainder = amount;
+
+            //number of coins to get the amount
+            decimal goldCoins = 0;
+            decimal silverCoins = 0;
+            decimal bronzeCoins = 0;
+
+
+            if (amount > goldValue)
+            {
+                // find the maximum number of gold coins that fit into the amount
+                goldCoins = Math.Floor(amount / goldValue);
+                remainder = Math.Floor(amount % goldValue);
+            }
+
+            if(remainder > silverValue)
+            {
+                silverCoins = Math.Floor(remainder / silverValue);
+                remainder %= silverValue;
+            }
+
+            if(remainder > bronzeValue)
+            {
+                bronzeCoins = Math.Floor(remainder / bronzeValue);
+                remainder %= bronzeCoins;
+            }
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {bronzeCoins}");
+            Console.WriteLine($"Remainder: {remainder}");
+
+        }
     }
-  }
 }
