58c43e47653d0f205d59125774de13796e1c03cf
Angélique Faye <angelique.d.faye@gmail.com>
2021-04-12 21:21:01 +0200

    first commit

---
+++ Coin Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Coin_Calculator
+{
+    class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter the amount to convert");
+      double amount = Math.Round(Convert.ToDouble(Console.ReadLine()));
+
+      Console.WriteLine($"{amount} cents is equal to...");
+
+      int goldValue = 10;
+      int silverValue = 5;
+
+      // find the maximum number of gold coins that fit into the amount
+      double goldCoins = Math.Floor(amount/goldValue);
+      double remainder = amount % goldValue;
+
+      // find the maximum number of silver coins that fit into the remainder
+      double silverCoins = Math.Floor(remainder / silverValue);
+      remainder = remainder % silverValue;
+
+      Console.WriteLine($"Gold coins: {goldCoins}");
+      Console.WriteLine($"Silver coins: {silverCoins}");
+      Console.WriteLine($"Remainder: {remainder}");
+
+    }
+  }
+}
